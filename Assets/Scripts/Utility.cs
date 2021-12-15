ad84b4fd2e1bf08c912189790e6b3e08a319a433
ACheesyBanjo <93452172+ACheesyBanjo@users.noreply.github.com>
2021-12-15 18:34:20 -0500

    added lots of comments
    COMMENTS FOR EVERYONE!!!!

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+// This file is authored by Hunter Bowers.
 class Utility : MonoBehaviour {
 
+    // This contains an easy way to reference direction for the
+    // movement function
     public enum Direction {
         FORWARD,
         BACKWARD, 
@@ -16,6 +18,8 @@ class Utility : MonoBehaviour {
         BACKWARD_RIGHT
     }
 
+    // This is a function to test using a separate class for
+    // utility functions
     public static string test() {
         return "This is a test string!";
     }
@@ -31,6 +35,8 @@ class Utility : MonoBehaviour {
     // -9 -8 -7
 
     // Its not a bug, Its a feature called a wrap around board...
+    // This function is meant to be able to move a piece in a given
+    // direction without having to do too many things by hand
     public static Vector3 moveToTile(Direction dir, int currentIndex) {
         switch (dir) {
             case Direction.FORWARD:
@@ -54,20 +60,36 @@ class Utility : MonoBehaviour {
         }
     }
 
+
+    // UNUSED FUNCTION
     // source: the index where the piece currently is before moving
     // destination: the index where the piece is going to move, if valid
     public static Vector3 moveToTile(int source, int destination) {
         // Check if index is a valid move
         return new Vector3(0,0,0);
     }
-
+    
+    // UNUSED FUNCTION
     public static int convertTileToCoordinates(int index) {
         return 0;
     }
 
+    // This function is used to be able to convert a given index into coordinate values
+    // for pieces in the game.
     public static Vector3 convertIndexToTile(int index) {
+        // This just takes the index, mods the value by 8 and applies some offsets
+        // that I spent a while tweaking
         float x = (index % 8 * 12.7f) + 5.5f;
+
+        // This variable was not so easy to figure out.
+        // Taking the modulus and applying some offsets would not work the same here
+        // it would essentially make the pieces move in a diagonal line
+        // What it does is convert the index to the horizontal row on the board
+        // using the property that the index divided by 8 gives the approximate row
+        // then flooring the value and applying the offsets gives us our Z position
         float z = (float)((System.Math.Floor(index/8.0) * 12.7f) + 5.5f);
+        
+        // And of course return the new vector
         return new Vector3(x, 1f, z);
     }
 
