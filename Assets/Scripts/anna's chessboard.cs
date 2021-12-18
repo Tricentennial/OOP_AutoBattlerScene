3b488b0fc813b2176e7315c6bbbb665ffd59d1f5
AnnaL0707 <95099513+AnnaL0707@users.noreply.github.com>
2021-12-18 00:27:29 -0500

    Update anna's chessboard.cs

--- Assets/Scripts/anna's chessboard.cs
+++ Assets/Scripts/anna's chessboard.cs
@@ -10,13 +10,16 @@ public class ChessBoard : MonoBehaviour
     [SerializeField] private float tileSize = 1.0f;
     [SerializeField] private float yOffset = 0.2f;
     [SerializeField] private Vector3 boardCenter = Vector3.zero;
-
+ 
+    // there are 64 tiles on the chess board, tile count x = 8 and tile count y = 8;
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
     private Vector3 bounds;
+
+    // creating a all of the tiles on the board
     private void Awake()
     {
         GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
@@ -75,13 +78,16 @@ public class ChessBoard : MonoBehaviour
     private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
-        tileObject.transform.parent = transform;
 
+        // chessboard transform, which helps when the art for chessboard is added to Unity.
+        //It helps, when you need to move the board, because the the tiles will move with the art piece of the board
+        tileObject.transform.parent = transform;
+        // creating a mesh
         Mesh mesh = new Mesh();
         tileObject.AddComponent<MeshFilter>().mesh = mesh;
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
-
+        //generating a single tile, by creating triangles.
         Vector3[] vertices = new Vector3[4];
         vertices[0] = new Vector3(x * tileSize, 0, y * tileSize) - bounds;
         vertices[1] = new Vector3(x * tileSize, 0, (y + 1) * tileSize) - bounds;
@@ -102,6 +108,7 @@ public class ChessBoard : MonoBehaviour
     }
 
     //Operations
+    //
     private Vector2Int LookupTileIndex(GameObject hitInfo)
     {
         for (int x = 0; x < TILE_COUNT_X; x++)
